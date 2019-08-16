542a53a95e68722c79ee13d9276f09454433e05c
zoraXT <451548768@qq.com>
2019-08-16 20:35:30 +0800

    遴选 test2 第二次提交

--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,14 @@
 export const test = {
 <<<<<<< HEAD
     test:'1',
+<<<<<<< HEAD
     test:'2'
 =======
     test:'1'
 >>>>>>> 127105c... test 第一次提交
+=======
+    test:'2',
+    test:'3',
+    name:'我是在test2分支上提交的'
+>>>>>>> b780e91... test2  分支第二次提交
 }
